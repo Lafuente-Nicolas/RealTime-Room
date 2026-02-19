843d72384f69bd3189f6269a4d3bd58545a58773
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2026-02-19 16:41:36 +0100

    feat (sass) : add minimal responsive

--- app/web/src/components/Hero/Hero.jsx
+++ app/web/src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ export default function Hero() {
     return (
         <section className="hero">
             <h1 className="hero__title">Bienvenue</h1>
-            <p className="hero__text">Prototype temps réel</p>
+            <p className="hero__text">En temps réel</p>
         </section>
     );
 }
