dd13b28d40f3bfb73228a911d0bcea12ab1a3e2f
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2026-02-20 14:41:39 +0100

    feat : remove folder

--- app/web/src/components/Hero/Hero.jsx
+++
@@ -1,8 +0,0 @@
-export default function Hero() {
-    return (
-        <section className="hero">
-            <h1 className="hero__title">Bienvenue</h1>
-            <p className="hero__text">En temps réel</p>
-        </section>
-    );
-}
