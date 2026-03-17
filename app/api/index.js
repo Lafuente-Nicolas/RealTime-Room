b2da6ce282e83f83f664ea746fb8788f85297c0a
Nicolas Lafuente <lafuentenicolas1@gmail.com>
2026-03-17 14:15:55 +0100

    feat : add the first game in the folder api

--- app/api/index.js
+++ app/api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
-import { playRound } from "../game-logic/game.js";
+import { playRound } from "./src/games/rockPaperScissors.js";
 
 const app = express();
 const server = http.createServer(app);
